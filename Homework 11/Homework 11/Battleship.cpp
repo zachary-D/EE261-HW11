760d75c99c1368f9d3c074b90afbf997594f75f6
Unknown <zacharyd1@live.com>
2017-11-15 10:56:18 -0500

    Created Solution

---
+++ Homework 11/Homework 11/Battleship.cpp
@@ -0,0 +1,4 @@
+
+
+//Idea: Don't actually store a 2-d grid of ships, store a vector of ships and instead have their beginning and ending points tracked as part of them.  If a shot falls within
+		//their area, count that as a shot.
